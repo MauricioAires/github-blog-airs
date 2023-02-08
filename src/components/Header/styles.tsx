02eaec40e643db9996fdbc90d8ffc97e471c4a84
MauricioAires <mauricioaires015@gmail.com>
2023-02-08 11:56:01 -0300

    style:(header) logo size

--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -3,25 +3,26 @@ import styled, { css } from 'styled-components'
 import Cover from '../../assets/cover.png'
 
 export const HeaderWrapper = styled.header`
-  ${({ theme }) => css`
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
 
-    background-image: url(${Cover});
-    background-repeat: no-repeat;
-    background-position: center center;
-  `}
+  background-image: url(${Cover});
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
 `
 
 export const HeaderContent = styled.div`
-  ${({ theme }) => css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 296px;
-    flex: 1;
-    max-width: 1440px;
-    padding: 1rem;
-  `}
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 296px;
+  flex: 1;
+  max-width: 1440px;
+  padding: 1rem;
+
+  > img {
+    height: 6rem;
+  }
 `
