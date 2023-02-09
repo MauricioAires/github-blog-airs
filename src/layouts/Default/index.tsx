3a62f9a68efb03a8d851f2b7f15c95a2a744d47c
MauricioAires <mauricioaires015@gmail.com>
2023-02-09 15:55:07 -0300

    feat:(layout) create

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -2,12 +2,16 @@ import { Outlet } from 'react-router-dom'
 import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
 
+import * as S from './styles'
+
 export function DefaultLayout() {
   return (
-    <>
+    <S.DefaultLayoutWrapper>
       <Header />
-      <Outlet />
+      <S.OutletWrapper>
+        <Outlet />
+      </S.OutletWrapper>
       <Footer />
-    </>
+    </S.DefaultLayoutWrapper>
   )
 }
