3bd290716e07483df33c4b5a146515eb6e9f36e9
MauricioAires <mauricioaires015@gmail.com>
2023-02-09 17:29:28 -0300

    feat:(post card) create component

---
+++ src/components/PostCard/index.tsx
@@ -0,0 +1,18 @@
+import * as S from './styles'
+
+export function PostCard() {
+  return (
+    <S.PostCardWrapper>
+      <header>
+        <span>JavaScript data types and data structures</span>
+        <time>Há 1 dia</time>
+      </header>
+
+      <p>
+        Programming languages all have built-in data structures, but these often
+        differ from one language to another. This article attempts to list the
+        built-in data structures available in
+      </p>
+    </S.PostCardWrapper>
+  )
+}
