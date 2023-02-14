8d0e72003b80b1775a0ee379c4979da1e29653a2
MauricioAires <mauricioaires015@gmail.com>
2023-02-14 19:24:48 -0300

    feat(home) fetch issues

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from 'react'
 import { PostCard } from '../../components/PostCard'
 import { Profile } from '../../components/Profile'
+import { api } from '../../libs/axios'
 
 import * as S from './styles'
 
+type Issue = {
+  id: number
+  body: string
+  title: string
+  created_at: string
+}
+interface FetchIssuesData {
+  total_count: number
+  items: Issue[]
+}
+
 export function HomePage() {
+  const [countIssues, setCountIssues] = useState(0)
+  const [issues, setIssues] = useState<Issue[]>([])
+  async function fetchIssues() {
+    const response = await api.get<FetchIssuesData>(
+      '/search/issues?q=repo:MauricioAires/github-blog-airs',
+    )
+
+    setCountIssues(response.data.total_count)
+    setIssues(response.data.items)
+  }
+
+  useEffect(() => {
+    fetchIssues()
+  }, [])
+
   return (
     <S.HomePageWrapper>
       <Profile />
@@ -12,18 +40,20 @@ export function HomePage() {
         <S.Title>
           <h1>Publicações</h1>
 
-          <span>6 publicações</span>
+          <span>{countIssues} publicações</span>
         </S.Title>
         <S.SearchForm>
           <input type="text" placeholder="Buscar conteúdo" />
         </S.SearchForm>
         <S.PostList>
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
+          {issues.map((issue) => (
+            <PostCard
+              key={issue.id}
+              body={issue.body}
+              createdAt={issue.created_at}
+              title={issue.title}
+            />
+          ))}
         </S.PostList>
       </S.HomeMain>
     </S.HomePageWrapper>
